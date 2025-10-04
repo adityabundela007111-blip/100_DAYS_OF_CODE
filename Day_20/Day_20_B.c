d747651efd23cc9d99de8f64338e7f09082a57df
Aditya Singh Bundela <adityabundela007111@gmail.com>
2025-10-04 19:59:52 +0530

    Create Day_20_B.c

---
+++ Day_20/Day_20_B.c
@@ -0,0 +1,30 @@
+Q40: Write a program to find the 1’s complement of a binary number and print it.
+
+/*
+Sample Test Cases:
+Input 1:
+1010
+Output 1:
+0101
+
+Input 2:
+1111
+Output 2:
+0000
+
+*/
+#include <stdio.h>
+int main(){
+    char ch;   
+    printf("Enter binary number: ");
+    while ((ch = getchar()) != '\n' && ch != EOF){
+        if (ch == '0'){
+            printf("1");
+        }
+        else if (ch == '1'){
+            printf("0");
+        }
+    }
+    printf("\n");
+    return 0;
+}
